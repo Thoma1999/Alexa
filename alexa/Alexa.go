ec1823aebcbdfb92ed68f2358de62199be03e8d4
thoma1999 <laryea.thomas@gmail.com>
2022-02-18 23:31:20 +0000

    Cut down

--- alexa/Alexa.go
+++ alexa/Alexa.go
@@ -4,92 +4,35 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func Stt(question interface{}) (map[string]interface{}, error) {
+func Service(jsonData interface{}, uri string, serviceName string) (map[string]interface{}, error) {
 	client := &http.Client{}
-	uri := "http://localhost:3002/stt"
-	if jsonspeech, err := json.Marshal(question); err == nil {
-		if req, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsonspeech)); err == nil {
-			if rsp, err := client.Do(req); err == nil {
-				if rsp.StatusCode == http.StatusOK {
-					t := map[string]interface{}{}
-					if err := json.NewDecoder(rsp.Body).Decode(&t); err == nil {
-						if q, ok := t["text"].(string); ok {
-							fmt.Println(q)
-						}
-						return t, nil
-					}
-				} else {
-					fmt.Println(rsp.StatusCode)
-				}
-			}
-		}
-	}
-	return nil, errors.New("Error converting speech to text")
-}
-
-func Alpha(question interface{}) (map[string]interface{}, error) {
-	client := &http.Client{}
-	uri := "http://localhost:3001/alpha"
-	if jsontext, err := json.Marshal(question); err == nil {
-		fmt.Println(string(jsontext))
-		if req, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsontext)); err == nil {
-			if rsp, err := client.Do(req); err == nil {
-				if rsp.StatusCode == http.StatusOK {
-					t := map[string]interface{}{}
-					if err := json.NewDecoder(rsp.Body).Decode(&t); err == nil {
-						if a, ok := t["text"].(string); ok {
-							fmt.Println(a)
-						}
-						return t, nil
-					}
-				} else {
-					fmt.Println(rsp.StatusCode)
-				}
-			}
-		}
-	}
-	return nil, errors.New("Error in answering query")
-}
-
-func Tts(answer interface{}) (map[string]interface{}, error) {
-	client := &http.Client{}
-	uri := "http://localhost:3003/tts"
-	if jsontext, err := json.Marshal(answer); err == nil {
+	if jsontext, err := json.Marshal(jsonData); err == nil {
 		if req, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsontext)); err == nil {
 			if rsp, err := client.Do(req); err == nil {
 				if rsp.StatusCode == http.StatusOK {
 					t := map[string]interface{}{}
 					if err := json.NewDecoder(rsp.Body).Decode(&t); err == nil {
 						return t, nil
-					} else {
-						fmt.Println(rsp.StatusCode)
 					}
 				}
 			}
 		}
 	}
-	return nil, errors.New("Error in answering query")
+	return nil, errors.New("Error in " + serviceName + " service")
 }
 
 func alexa(w http.ResponseWriter, r *http.Request) {
-	t := map[string]interface{}{}
-	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
-		if question, err := Stt(t); err == nil {
-			if answer, err := Alpha(question); err == nil {
-				if speechA, err := Tts(answer); err == nil {
-					if audio, ok := speechA["speech"].(string); ok {
+	speechInput := map[string]interface{}{}
+	if err := json.NewDecoder(r.Body).Decode(&speechInput); err == nil {
+		if question, err := Service(speechInput, "http://localhost:3002/stt", "Speech to text"); err == nil {
+			if answer, err := Service(question, "http://localhost:3001/alpha", "Wolfram Alpha"); err == nil {
+				if speechOutput, err := Service(answer, "http://localhost:3003/tts", "Text to speech"); err == nil {
+					if audio, ok := speechOutput["speech"].(string); ok {
 						u := map[string]interface{}{"speech": audio}
 						w.WriteHeader(http.StatusOK)
 						json.NewEncoder(w).Encode(u)
